5eb2799d3c1a82298d7411e36b6208cd85782f43
Neeiser <stortini.study@gmail.com>
2022-03-24 17:04:08 +0100

    axios structure + npm install

--- src/components/MainSpotify.vue
+++ src/components/MainSpotify.vue
@@ -1,18 +1,34 @@
 <template>
     <main>
-         
+        <div class="container">
+            
+        </div> 
     </main>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name:'MainSpotify',
-    /* data () {
-        return
-    } */
+     data () {
+        return{
+            arrChar:[],
+        }
+    },
+    created(){
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then(function(response) {
+            this.arrChar = response.data.response
+        })
+    }
 }
+
 </script>
 
 <style scoped lang="scss">
-
+@import "../assets/styles/partials/variables.scss";
+main{
+    background-color: $blueMain;
+}
 </style>
